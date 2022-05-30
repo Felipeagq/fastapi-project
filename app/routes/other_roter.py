5a00f28d5b83266835eb9a38466a7dbeb8addf6a
Felipeagq <felipeagq99@gmail.com>
2022-05-30 17:58:43 -0500

    [MODIFY] fastapi-utils repeat_every tasks

--- app/routes/other_roter.py
+++ app/routes/other_roter.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from app.schemas import user_schema
 from app.utils.security import get_current_user
 
+from fastapi_utils.tasks import repeat_every
 
 router = APIRouter()
 
@@ -9,5 +10,16 @@ router = APIRouter()
 def retorno(
     get_current_user:user_schema.UserRequestSchema = Depends(get_current_user)
 ):
-    print(get_current_user)
     return  get_current_user
+
+
+
+@router.on_event("startup")
+@repeat_every(seconds=60)
+def repeated_task() -> str:
+    """
+    Internal Task that start on the startup event.
+    Must dont have arguments.
+    Repeat every "n" seconds
+    """
+    print("imprimiendo")
